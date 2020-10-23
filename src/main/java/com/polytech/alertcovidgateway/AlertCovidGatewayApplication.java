58b51374895153a1372a9f9e3eedacd392798ca1
Remi Salmi <remi.salmi@gmail.com>
2020-10-23 15:15:01 +0200

    first commit

---
+++ src/main/java/com/polytech/alertcovidgateway/AlertCovidGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertCovidGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertCovidGatewayApplication.class, args);
+	}
+
+}
